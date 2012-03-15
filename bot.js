94582f8419e298b85d33d0dd064f5f7c8fd87b61
Jeff Leombruno <leombruno@gmail.com>
2012-03-15 00:08:11 -0400

    starting refactor to make bot functions an included module

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+var Bot    = require('ttapi');
 
 var config = {
 	botGreetings : ['botname '],
@@ -8,8 +8,48 @@ var config = {
 	userid 		: 'XXXXXXXXXXXXXXXXXXXXXXXX',
 	roomid 		: 'XXXXXXXXXXXXXXXXXXXXXXXX',	
 	port		: 1337
-
-
 };
 
-eval(fs.readFileSync('./lib/http.js')+'');
+config.autobop = false;
+config.autobot = false;
+config.autoDj = true;
+config.danceMode = false;
+config.followMe = false;
+config.debug = false;
+config.holdMode = false;
+config.watchMode = false;
+config.queue = false;
+
+var botStuff = require('./lib/http.js');
+
+
+var bot = new Bot(config.auth, config.userid);
+bot.listen(config.port, '127.0.0.1');
+
+
+var botObj = {
+	'eventData' : null,
+	'config' : config,
+	'bot' : bot
+}
+
+
+bot.on('ready', function (data) {
+	botObj.eventData = data;
+	botStuff.onReady(botObj);
+});
+
+bot.on('deregistered', function (data) {
+	botObj.eventData = data;
+	botStuff.onDeregistered(botObj);
+});
+
+bot.on('roomChanged', function (data) {
+	botObj.eventData = data;
+	botStuff.onRoomChanged(botObj);
+});
+
+bot.on('booted_user', function (data) {
+	botObj.eventData = data;
+	botStuff.onBootedUser(botObj);
+});
