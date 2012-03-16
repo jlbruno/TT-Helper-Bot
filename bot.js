0b0d0a7bc57f4fab336511e4236e71566363f1b5
Jeff Leombruno <leombruno@gmail.com>
2012-03-16 00:24:09 -0400

    updated bot file

--- bot.js
+++ bot.js
@@ -7,18 +7,18 @@ var config = {
 	auth 		: 'auth+live+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
 	userid 		: 'XXXXXXXXXXXXXXXXXXXXXXXX',
 	roomid 		: 'XXXXXXXXXXXXXXXXXXXXXXXX',	
-	port		: 1337
+	port		: 1337,
+	autobop   : false,
+	autobot   : false,
+	autoDj    : true,
+	danceMode : false,
+	followMe  : false,
+	debug     : false,
+	holdMode  : false,
+	watchMode : false,
+	queue     : false
 };
 
-config.autobop = false;
-config.autobot = false;
-config.autoDj = true;
-config.danceMode = false;
-config.followMe = false;
-config.debug = false;
-config.holdMode = false;
-config.watchMode = false;
-config.queue = false;
 
 var botStuff = require('./lib/http.js');
 
@@ -28,28 +28,9 @@ bot.listen(config.port, '127.0.0.1');
 
 
 var botObj = {
-	'eventData' : null,
 	'config' : config,
 	'bot' : bot
 }
 
 
-bot.on('ready', function (data) {
-	botObj.eventData = data;
-	botStuff.onReady(botObj);
-});
-
-bot.on('deregistered', function (data) {
-	botObj.eventData = data;
-	botStuff.onDeregistered(botObj);
-});
-
-bot.on('roomChanged', function (data) {
-	botObj.eventData = data;
-	botStuff.onRoomChanged(botObj);
-});
-
-bot.on('booted_user', function (data) {
-	botObj.eventData = data;
-	botStuff.onBootedUser(botObj);
-});
+botStuff.init(botObj);
