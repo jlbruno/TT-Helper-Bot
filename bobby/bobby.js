9b455f96f84dff84e39c88977b7a4858a5dcf695
Jeff L <bruno1378@gmail.com>
2021-03-18 14:03:10 -0400

    Test bot so I can make code changes without taking main bot down

---
+++ bobby/bobby.js
@@ -0,0 +1,68 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
+var Bot    = require('ttapi');
+
+// note: greetings need the space after to match properly
+// unsure if the second greeting without space is needed
+var config = {
+	botGreetings : ["bobby ", "@BobbyO'Toole "],
+	botGreetingsMod	: ['bott ', 'bott'],
+	botOwner 	: process.env.TTFM_CONFIG_OWNER,
+	auth 		: process.env.TTFM_CONFIG_AUTH,
+	userid 		: process.env.TTFM_CONFIG_USERID,
+	roomid 		: process.env.TTFM_CONFIG_ROOMID,	
+	server_port	: process.env.HTTP_PORT,
+	server_host : process.env.HTTP_HOST || '0.0.0.0',
+	autobop   : false,
+	autobot   : false,
+	autoDj    : false,
+	danceMode : true,
+	danceVotesReq: 1,
+	followMe  : false,
+	debug     : false,
+	holdMode  : false,
+	watchMode : false,
+	queue     : false,
+	welcomeMods : true,  // TODO true if you want the bot to PM mods when they join
+	welcomeAll  : false,  // TODO true if you want the bot to PM 
+	lastfm    : {
+		api_key: "LASTFM_KEY",
+		secret: "LASTFM_SECRET",
+		useragent: "APP_STRING"
+	}
+};
+
+var botBase = require('../lib/bot-base.js');
+var bot = new Bot(config.auth, config.userid);
+bot.debug = false; /* this can be set to true to put ttapi into debug mode */
+
+
+
+var botObj = {
+	'config' : config,
+	'bot' : bot
+}
+
+/* ===== REQUIRED MODULES ====== */
+// init base bot
+botBase.init(botObj);
+
+console.log(botObj);
+
+/* ===== OPTIONAL MODULES ===== */
+// init server listening
+/*
+var httpServer = require('../lib/server-http.js');
+botObj.commands = botBase.commands;
+httpServer.init(botObj);
+bot.listen(config.server_port, config.server_host);
+*/
+
+
+// bot will speak the release date of the album the song is from
+/*
+var botLastFM = require('./lib/module-lastfm.js');
+botLastFM.init(botObj);
+*/
